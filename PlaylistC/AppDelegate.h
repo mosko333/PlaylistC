af3bd394e1d337bd5de997fb61de5e5830273197
AdamAM <adamam@gmail.com>
2018-06-04 10:21:18 -0600

    Initial Commit

---
+++ PlaylistC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlaylistC
+//
+//  Created by Adam on 04/06/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlaylistC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlaylistC
+//
+//  Created by Adam on 04/06/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
